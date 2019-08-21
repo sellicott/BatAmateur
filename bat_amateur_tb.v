848ddbdce319541818fa2ffccc1fe0f21cbe622a
Ellicott <ellicott.4@coeit.osu.edu>
2019-08-21 17:04:23 -0400

    fix compile errors in test bench

--- bat_amateur_tb.v
+++ bat_amateur_tb.v
@@ -11,16 +11,17 @@ wire [ADDRESS_WIDTH-1:0] ADDRESS_BUS;
 wire [15:0] OUTPUT_BUS;
 
 bat_amateur my_bat_amateur (
-    .HALT(HALT)
+    .HALT(HALT),
     .CLK(CLK),
-    .RESET(RESET),
-    .DATA_BUS(DATA_BUS),
-    .ADDRESS_BUS(ADDRESS_BUS),
-    .OUTPUT_BUS(OUTPUT_BUS)
+    .RST(RESET),
+    .RAM_EN(HALT),
+    .DATA(DATA_BUS),
+    .ADDRESS(ADDRESS_BUS),
+    .OUT(OUTPUT_BUS)
 );
 
 bat_amateur_stim my_bat_amateur_stim (
-    .HALT(HALT)
+    .HALT(HALT),
     .CLK(CLK),
     .RESET(RESET),
     .DATA_BUS(DATA_BUS),
