6a348aaade22319418f98a533d648c6518c0ade1
Ellicott <ellicott.4@coeit.osu.edu>
2019-08-22 18:09:58 -0400

    Updated top level to match controller

--- BatAmaTop.v
+++ BatAmaTop.v
@@ -10,32 +10,54 @@ module bat_amateur (
 wire FORCE_RAM_RW;
 wire FORCE_RAM_EN;
 wire [15:0] BUS;
-wire [23:0] REGS;
 wire [7:0]  ALU_REG;
 wire [15:0] INSTR;
-wire [8:0]  CTRL_REGS;
-wire [5:0]  ALU_CTRL;
 wire [15:0] ALU_IN1, ALU_IN2;
+
+wire PC_INC, PC_RW, PC_EN;
+wire MAR_LOAD, MAR_EN;
+wire RAM_RW, RAM_EN;
+wire IR_LOAD, IR_EN;
+wire ALU_EN;
+wire [4:0] ALU_OP;
+
+wire [7:0] REGS_INC;
+wire [7:0] REGS_RW;
+wire [7:0] REGS_EN;
 
 BatAmateurController CTRL(
     .CLK(!CLK & !HALT), 
     .ALU_REG(ALU_REG),
     .RST(RST), 
     .INSTR(INSTR), 
-    .PC(CTRL_REGS[2:0]), 
-    .MAR(CTRL_REGS[4:3]), 
-    .RAM(CTRL_REGS[6:5]), 
-    .IR(CTRL_REGS[8:7]), 
-    .REGS(REGS), 
-    .ALU_EN(ALU_CTRL[0]),
-    .ALU_OP(ALU_CTRL[5:1])
+
+	.PC_INC(PC_INC),
+	.PC_RW(PC_RW),
+	.PC_EN(PC_EN),
+
+    .MAR_LOAD(MAR_LOAD),
+	.MAR_EN(MAR_EN),
+
+	.RAM_RW(RAM_RW),
+	.RAM_EN(RAM_EN),
+	
+	.IR_LOAD(IR_LOAD),
+	.IR_EN(IR_EN),
+
+	//A, B, 3, 4, 5, 6, 7, OUT (from small to big)
+	.REGS_INC(REGS_INC),
+	.REGS_RW(REGS_RW),
+	.REGS_EN(REGS_EN),
+
+    .ALU_EN(ALU_EN),
+    .ALU_OP(ALU_OP)
 );
 
 alu ALU(
     .in_1(ALU_IN1), 
     .in_2(ALU_IN2), 
-    .select(ALU_CTRL[3:1]), 
-    .enable(ALU_CTRL[0]), 
+    .select(ALU_OP[3:0]), 
+    .enable(ALU_EN), 
     .carry_in(1'b0),
     .data(BUS), 
     .carry_out(ALU_REG[1]), 
@@ -46,19 +68,40 @@ register #(.COUNT_EN(0))
 MAR_REG (
     .RESET(RST), 
     .CLOCK(CLK), 
-    .LOAD(CTRL_REGS[4]), 
-    .ENABLE(CTRL_REGS[3] & !HALT),
+    .LOAD(MAR_LOAD), 
+    .ENABLE(MAR_EN & !HALT),
     .DATA_IN(BUS), 
     .DATA_OUT(ADDRESS),
+    .COUNT(1'b0)
+);
+
+bidi_register PC(
+    .RESET(RST), 
+    .CLOCK(CLK), 
+    .RW(PC_RW), 
+    .ENABLE(PC_EN), 
+    .COUNT(PC_INC),
+    .DATA(BUS)
+);
+
+register_ir #(.COUNT_EN(0)) 
+IR_REG(
+    .RESET(RST), 
+    .CLOCK(CLK), 
+    .LOAD(IR_LOAD), 
+    .ENABLE(IR_EN),
+    .DATA_IN(BUS), 
+    .DATA_OUT(BUS), 
+    .INSTRUCTION_OUT(INSTR),
     .COUNT(1'b0)
 );
 
 bidi_register_output A_REG(
     .RESET(RST), 
     .CLOCK(CLK), 
-    .RW(REGS[1]), 
-    .ENABLE(REGS[0]), 
-    .COUNT(REGS[2]),
+    .RW(REGS_RW[0]), 
+    .ENABLE(REGS_EN[0]), 
+    .COUNT(REGS_INC[0]),
     .DATA(BUS), 
     .OUTPUT(ALU_IN1)
 );
@@ -66,79 +109,70 @@ bidi_register_output A_REG(
 bidi_register_output B_REG(
     .RESET(RST), 
     .CLOCK(CLK), 
-    .RW(REGS[4]), 
-    .ENABLE(REGS[3]), 
-    .COUNT(REGS[5]),
+    .RW(REGS_RW[1]), 
+    .ENABLE(REGS_EN[1]), 
+    .COUNT(REGS_INC[1]),
     .DATA(BUS), 
     .OUTPUT(ALU_IN2)
-);
-
-bidi_register PC(
-    .RESET(RST), 
-    .CLOCK(CLK), 
-    .RW(CTRL_REGS[1]), 
-    .ENABLE(CTRL_REGS[0]), 
-    .COUNT(CTRL_REGS[2]),
-    .DATA(BUS)
 );
 
 bidi_register THREE(
     .RESET(RST), 
     .CLOCK(CLK), 
-    .RW(REGS[7]), 
-    .ENABLE(REGS[6]), 
-    .COUNT(REGS[8]), 
+    .RW(REGS_RW[2]), 
+    .ENABLE(REGS_EN[2]), 
+    .COUNT(REGS_INC[2]),
     .DATA(BUS)
 );
 
 bidi_register FOUR(
     .RESET(RST), 
     .CLOCK(CLK), 
-    .RW(REGS[10]), 
-    .ENABLE(REGS[9]), 
-    .COUNT(REGS[11]), 
+    .RW(REGS_RW[3]), 
+    .ENABLE(REGS_EN[3]), 
+    .COUNT(REGS_INC[3]),
     .DATA(BUS)
 );
 
 bidi_register FIVE(
-   .RESET(RST), 
-   .CLOCK(CLK), 
-   .RW(REGS[13]), 
-   .ENABLE(REGS[12]), 
-   .COUNT(REGS[14]), 
-   .DATA(BUS)
+    .RESET(RST), 
+    .CLOCK(CLK), 
+    .RW(REGS_RW[4]), 
+    .ENABLE(REGS_EN[4]), 
+    .COUNT(REGS_INC[4]),
+    .DATA(BUS)
 );
 
 bidi_register SIX(
     .RESET(RST), 
     .CLOCK(CLK), 
-    .RW(REGS[16]), 
-    .ENABLE(REGS[15]), 
-    .COUNT(REGS[17]), 
+    .RW(REGS_RW[5]), 
+    .ENABLE(REGS_EN[5]), 
+    .COUNT(REGS_INC[5]),
     .DATA(BUS)
 );
 
 bidi_register SEVEN(
     .RESET(RST), 
     .CLOCK(CLK), 
-    .RW(REGS[19]), 
-    .ENABLE(REGS[18]), 
-    .COUNT(REGS[20]), 
+    .RW(REGS_RW[6]), 
+    .ENABLE(REGS_EN[6]), 
+    .COUNT(REGS_INC[6]),
     .DATA(BUS)
 );
 
 bidi_register_output OUTREG(
     .RESET(RST), 
     .CLOCK(CLK), 
-    .RW(REGS[22]), 
-    .ENABLE(REGS[21]), 
-    .COUNT(REGS[23]), 
+    .RW(REGS_RW[7]), 
+    .ENABLE(REGS_EN[7]), 
+    .COUNT(REGS_INC[7]),
     .DATA(BUS), 
     .OUTPUT(OUT)
 );
 
-assign FORCE_RAM_RW = (HALT) ? RAM_RW : CTRL_REGS[6];
-assign FORCE_RAM_EN = (HALT) ? RAM_EN : CTRL_REGS[5];
+assign FORCE_RAM_RW = (HALT) ? RAM_RW : RAM_RW;
+assign FORCE_RAM_EN = (HALT) ? RAM_EN : RAM_EN;
 memory RAM(
     .address(ADDRESS), 
     .clk(CLK), 
@@ -146,18 +180,5 @@ memory RAM(
 	.enable(FORCE_RAM_EN), 
     .reset(RST), 
     .data(BUS)
-);
-
-register_ir #(.COUNT_EN(0)) 
-IR_REG(
-    .RESET(RST), 
-    .CLOCK(CLK), 
-    .LOAD(CTRL_REGS[8]), 
-    .ENABLE(CTRL_REGS[7]),
-    .DATA_IN(BUS), 
-    .DATA_OUT(BUS), 
-    .INSTRUCTION_OUT(INSTR),
-    .COUNT(1'b0)
-
 );
 endmodule
