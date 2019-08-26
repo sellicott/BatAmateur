64ec6853335a7f34f70bd5c9952c51d1dad72c13
Sam Ellicott <sellicott@cedarville.edu>
2019-08-26 13:28:02 -0400

    add some more displays

--- BatAmateurController.v
+++ BatAmateurController.v
@@ -69,282 +69,287 @@ begin
 	//now we configure the signals correctly
 	case(uOP)
 	
-		0: //initial retrieval
-		begin
-			PC_INC <= 1'b0;
-			PC_RW  <= 1'b1;
-			PC_EN  <= 1'b1;
+  0: //initial retrieval
+  begin
+    $display("uOP 0: fetch");
+    PC_INC <= 1'b0;
+    PC_RW  <= 1'b1;
+    PC_EN  <= 1'b1;
 
-			MAR_LOAD <= 1'b1;
-		end
+    MAR_LOAD <= 1'b1;
+  end
 
-		1: //load from memory into IR, incriement PC
-		begin
-			PC_INC <= 1'b1;
-			PC_RW <= 1'b0;
-			PC_EN <= 1'b0;
+  1: //load from memory into IR, incriement PC
+  begin
+    $display("uOP 1: decode");
+    PC_INC <= 1'b1;
+    PC_RW <= 1'b0;
+    PC_EN <= 1'b0;
 
-			RAM_RW <= 1'b1;
-			RAM_EN <= 1'b1;
+    RAM_RW <= 1'b1;
+    RAM_EN <= 1'b1;
 
-			IR_LOAD <= 1'b1;
-		end
-	
-		2: //and he, upon, look'd his creation,
-		   //wept, for it was to be ugly and dumb
-		   //yet, he worked on, as if to be unfazed
-		   //he'd finish if it cost him ARM and THUMB
-		   // Register instructions 
-		begin
-			if(INSTR[15:12] == 4'b0111)
-			begin
-        $display("register instruction");
-				//this is the pit of ALU
-				//tread lightly, and with a big 
-				
-				//check MOV
-				if(INSTR[11:7] == 5'b11111)
-				begin
-          $display("MOV instruction");
-					//r1 enable
-					REGS_EN[INSTR[5:3]] <= 1'b1;
-					REGS_RW[INSTR[5:3]] <= 1'b0;
-					//r2 enable and read out
-					REGS_EN[INSTR[2:0]] <= 1'b1;
-					REGS_RW[INSTR[2:0]] <= 1'b1;
-					//reset
-					uOP <= 3'b111;
-				end
-				//check inc
-				else if(INSTR[11:7] == 5'b11110)
-				begin
-          $display("INC instruction");
-					//r1 inc and read out (no enable)
-					REGS_INC[INSTR[5:3]] <= 1'b1;
-					REGS_RW[INSTR[5:3]] <= 1'b0;
-					//reset
-					uOP <= 3'b111;
-				end
-				else
-				begin
-          $display("ALU instruction");
-					// move from op1 to register A if op1 is not register A
-					if (INSTR[5:3] != 3'h0) begin
-            $display("move op1 to A");
-						REGS_EN[0] <= 1'b1;
-						REGS_RW[0] <= 1'b0; // write reg A
-						REGS_EN[INSTR[5:3]] <= 1'b1;
-						REGS_RW[INSTR[5:3]] <= 1'b1; // read op1
-					end
-					// otherwise do nothing
-				end
-			end
-			else if (INSTR[15:12] == 4'b0111)
-			begin
-				// NOP instruction
-				uOP <= 3'b111;
-				$display("NOP instruction");
-			end
-			else
-			begin
-				//this is the memory den
-				//oh no.
-				// direct jump instruction
-				if(INSTR[15] == 1'b0 && INSTR[14]==1'b1) 
-				begin
-					$display("JMP instruction");
-					//00 is uncond, 01 is z = 1, 10 is z = 0, 11 is unregistered
-					//i guess 11 is unconditional unjump lmao
-					if ( 
-						(INSTR[13:12] == 2'd0) || // branch unconditional
-						(INSTR[13:12] == 2'd1 && ALU_REG[0] == 1'b0) || // branch if zero
-						(INSTR[13:12] == 2'd2 && ALU_REG[0] == 1'b1) // branch if one
-					) 
-					begin
-						// move the location (stored in the instruction register) 
-						// to the program counter
-						IR_EN <= 1'b1;
-						IR_LOAD <= 1'b0; 
-						PC_INC <= 1'b0;
-						PC_EN <= 1'b1;
-						PC_RW <= 1'b0; // write
+    IR_LOAD <= 1'b1;
+  end
 
-					end 
-					// otherwise incriment the program counter
-					else begin
-						PC_INC <= 1'b1;
-					end
-					uOP <= 3'b111;
-				end
-				else begin
-					$display("STORE or LOAD instruction");
-					// write addr to mar 
-					// we can use it for direct or indirect addressing
-					IR_LOAD <= 1'b0;
-					IR_EN <= 1'b1;
-					MAR_LOAD <= 1'b1;
-				end
-			end
-		end
-		3:
-		begin
-			if(INSTR[15:12] == 4'b0111)
-			begin
+  2: //and he, upon, look'd his creation,
+      //wept, for it was to be ugly and dumb
+      //yet, he worked on, as if to be unfazed
+      //he'd finish if it cost him ARM and THUMB
+      // Register instructions 
+  begin
+    $display("uOP 2: execute");
+    if(INSTR[15:12] == 4'b0111)
+    begin
+      $display("register instruction");
+      //this is the pit of ALU
+      //tread lightly, and with a big 
+      
+      //check MOV
+      if(INSTR[11:7] == 5'b11111)
+      begin
+        $display("MOV instruction");
+        //r1 enable
+        REGS_EN[INSTR[5:3]] <= 1'b1;
+        REGS_RW[INSTR[5:3]] <= 1'b0;
+        //r2 enable and read out
+        REGS_EN[INSTR[2:0]] <= 1'b1;
+        REGS_RW[INSTR[2:0]] <= 1'b1;
+        //reset
+        uOP <= 3'b111;
+      end
+      //check inc
+      else if(INSTR[11:7] == 5'b11110)
+      begin
+        $display("INC instruction");
+        //r1 inc and read out (no enable)
+        REGS_INC[INSTR[5:3]] <= 1'b1;
+        REGS_RW[INSTR[5:3]] <= 1'b0;
+        //reset
+        uOP <= 3'b111;
+      end
+      else
+      begin
         $display("ALU instruction");
-				// move from op2 to register B if op2 is not register B
-				if (INSTR[2:0] != 3'h0) 
-				begin
-          $display("move op2 to B");
-					REGS_EN[1] <= 1'b1;
-					REGS_RW[1] <= 1'b0; // write reg A
-					REGS_EN[INSTR[2:0]] <= 1'b1;
-					REGS_RW[INSTR[2:0]] <= 1'b1; // read op1
-				end
-				// otherwise do nothing
+        // move from op1 to register A if op1 is not register A
+        if (INSTR[5:3] != 3'h0) begin
+          $display("move op1 to A");
+          REGS_EN[0] <= 1'b1;
+          REGS_RW[0] <= 1'b0; // write reg A
+          REGS_EN[INSTR[5:3]] <= 1'b1;
+          REGS_RW[INSTR[5:3]] <= 1'b1; // read op1
+        end
+        // otherwise do nothing
+      end
+    end
+    else if (INSTR[15:12] == 4'b1111)
+    begin
+      // NOP instruction
+      uOP <= 3'b111;
+      $display("NOP instruction");
+    end
+    else
+    begin
+      //this is the memory den
+      //oh no.
+      // direct jump instruction
+      if(INSTR[15] == 1'b0 && INSTR[14]==1'b1) 
+      begin
+        $display("JMP instruction");
+        //00 is uncond, 01 is z = 1, 10 is z = 0, 11 is unregistered
+        //i guess 11 is unconditional unjump lmao
+        if ( 
+          (INSTR[13:12] == 2'd0) || // branch unconditional
+          (INSTR[13:12] == 2'd1 && ALU_REG[0] == 1'b0) || // branch if zero
+          (INSTR[13:12] == 2'd2 && ALU_REG[0] == 1'b1) // branch if one
+        ) 
+        begin
+          // move the location (stored in the instruction register) 
+          // to the program counter
+          IR_EN <= 1'b1;
+          IR_LOAD <= 1'b0; 
+          PC_INC <= 1'b0;
+          PC_EN <= 1'b1;
+          PC_RW <= 1'b0; // write
 
-			// Memory instructions
-			end 
-			else 
-			begin
-				// check for indirect instructions
-				if (INSTR[15] == 1'b1) begin
-					//indirect JMP set of instructions
-					if(INSTR[14]==1'b1) 
-					begin
-						$display("JMP instruction");
-						//00 is uncond, 01 is z = 1, 10 is z = 0, 11 is unregistered
-						//i guess 11 is unconditional unjump lmao
-						if ( 
-							(INSTR[13:12] == 2'd0) || // branch unconditional
-							(INSTR[13:12] == 2'd1 && ALU_REG[0] == 1'b0) || // branch if zero
-							(INSTR[13:12] == 2'd2 && ALU_REG[0] == 1'b1) // branch if one
-						) 
-						begin
-							// move the location (stored in the RAM) 
-							// to the program counter
-							RAM_EN <= 1'b1;
-							RAM_RW <= 1'b1; // read
-							PC_INC <= 1'b0;
-							PC_EN <= 1'b1;
-							PC_RW <= 1'b0; // write
+        end 
+        // otherwise incriment the program counter
+        else begin
+          PC_INC <= 1'b1;
+        end
+        uOP <= 3'b111;
+      end
+      else begin
+        $display("STORE or LOAD instruction");
+        // write addr to mar 
+        // we can use it for direct or indirect addressing
+        IR_LOAD <= 1'b0;
+        IR_EN <= 1'b1;
+        MAR_LOAD <= 1'b1;
+      end
+    end
+  end
+  3:
+  begin
+    $display("uOP 3: execute");
+    if(INSTR[15:12] == 4'b0111)
+    begin
+      $display("ALU instruction");
+      // move from op2 to register B if op2 is not register B
+      if (INSTR[2:0] != 3'h0) 
+      begin
+        $display("move op2 to B");
+        REGS_EN[1] <= 1'b1;
+        REGS_RW[1] <= 1'b0; // write reg A
+        REGS_EN[INSTR[2:0]] <= 1'b1;
+        REGS_RW[INSTR[2:0]] <= 1'b1; // read op1
+      end
+      // otherwise do nothing
 
-						end 
-						// otherwise incriment the program counter
-						else begin
-							PC_INC <= 1'b1;
-						end
-						uOP <= 3'b111;
-					end
-					else begin
-						// move the data from the memory back to MAR
-						MAR_LOAD <= 1'b1;
-						RAM_EN <= 1'b1;
-						RAM_RW <= 1'b1; //read
-					end
-				end
-				// store instruction
-				else if (INSTR[13] == 1'b1)
-				begin
-					$display("STORE instruction");
-					// store A
-					if (INSTR[12] == 1'b0) begin
-						REGS_EN[0] <= 1'b1;
-						REGS_RW[0] <= 1'b1; //read
-					end
-					// store B
-					else begin
-						REGS_EN[1] <= 1'b1;
-						REGS_RW[1] <= 1'b1; //read
-					end
+    // Memory instructions
+    end 
+    else 
+    begin
+      // check for indirect instructions
+      if (INSTR[15] == 1'b1) begin
+        //indirect JMP set of instructions
+        if(INSTR[14]==1'b1) 
+        begin
+          $display("JMP instruction");
+          //00 is uncond, 01 is z = 1, 10 is z = 0, 11 is unregistered
+          //i guess 11 is unconditional unjump lmao
+          if ( 
+            (INSTR[13:12] == 2'd0) || // branch unconditional
+            (INSTR[13:12] == 2'd1 && ALU_REG[0] == 1'b0) || // branch if zero
+            (INSTR[13:12] == 2'd2 && ALU_REG[0] == 1'b1) // branch if one
+          ) 
+          begin
+            // move the location (stored in the RAM) 
+            // to the program counter
+            RAM_EN <= 1'b1;
+            RAM_RW <= 1'b1; // read
+            PC_INC <= 1'b0;
+            PC_EN <= 1'b1;
+            PC_RW <= 1'b0; // write
 
-					RAM_EN <= 1'b1;
-					RAM_RW <= 1'b0; // write
-					uOP <= 3'b111;
-				end
-				// load instruction
-				else 
-				begin
-					$display("LOAD instruction");
-					// load A
-					if (INSTR[12] == 1'b0) begin
-						REGS_EN[0] <= 1'b1;
-						REGS_RW[0] <= 1'b0; // write
-					end
-					// load B
-					else begin
-						REGS_EN[1] <= 1'b1;
-						REGS_RW[1] <= 1'b0; // write
-					end
+          end 
+          // otherwise incriment the program counter
+          else begin
+            PC_INC <= 1'b1;
+          end
+          uOP <= 3'b111;
+        end
+        else begin
+          // move the data from the memory back to MAR
+          MAR_LOAD <= 1'b1;
+          RAM_EN <= 1'b1;
+          RAM_RW <= 1'b1; //read
+        end
+      end
+      // store instruction
+      else if (INSTR[13] == 1'b1)
+      begin
+        $display("STORE instruction");
+        // store A
+        if (INSTR[12] == 1'b0) begin
+          REGS_EN[0] <= 1'b1;
+          REGS_RW[0] <= 1'b1; //read
+        end
+        // store B
+        else begin
+          REGS_EN[1] <= 1'b1;
+          REGS_RW[1] <= 1'b1; //read
+        end
 
-					RAM_EN <= 1'b1;
-					RAM_RW <= 1'b1; // read 
-					uOP <= 3'b111;
-				end
-			end
-		end
-		4:
-		begin
-			// register operation
-			if(INSTR[15:12] == 4'b0111)
-			begin
-				ALU_EN <= 1'b1;
-				ALU_OP <= INSTR[11:7];
-				// select what accumulator to write to
-				if (INSTR[6]) begin
-					REGS_EN[0] <= 1'b1;
-					REGS_RW[0] <= 1'b0; //write
-				end else begin
-					REGS_EN[1] <= 1'b1;
-					REGS_RW[1] <= 1'b0; //write
-				end
-				uOP <= 3'b111;
-			end
-			// memory operation
-			else
-			begin
-				// store instruction
-				if (INSTR[13] == 1'b1)
-				begin
-					// store A
-					if (INSTR[12] == 1'b0) begin
-						REGS_EN[0] <= 1'b1;
-						REGS_RW[0] <= 1'b1; //read
-					end
-					// store B
-					else begin
-						REGS_EN[1] <= 1'b1;
-						REGS_RW[1] <= 1'b1; //read
-					end
+        RAM_EN <= 1'b1;
+        RAM_RW <= 1'b0; // write
+        uOP <= 3'b111;
+      end
+      // load instruction
+      else 
+      begin
+        $display("LOAD instruction");
+        // load A
+        if (INSTR[12] == 1'b0) begin
+          REGS_EN[0] <= 1'b1;
+          REGS_RW[0] <= 1'b0; // write
+        end
+        // load B
+        else begin
+          REGS_EN[1] <= 1'b1;
+          REGS_RW[1] <= 1'b0; // write
+        end
 
-					RAM_EN <= 1'b1;
-					RAM_RW <= 1'b0; // write
-					uOP <= 3'b111;
-				end
-				// load instruction
-				else 
-				begin
-					// load A
-					if (INSTR[12] == 1'b0) begin
-						REGS_EN[0] <= 1'b1;
-						REGS_RW[0] <= 1'b0; // write
-					end
-					// load B
-					else begin
-						REGS_EN[1] <= 1'b1;
-						REGS_RW[1] <= 1'b0; // write
-					end
+        RAM_EN <= 1'b1;
+        RAM_RW <= 1'b1; // read 
+        uOP <= 3'b111;
+      end
+    end
+  end
+  4:
+  begin
+    $display("uOP 3: execute");
+    // register operation
+    if(INSTR[15:12] == 4'b0111)
+    begin
+      ALU_EN <= 1'b1;
+      ALU_OP <= INSTR[11:7];
+      // select what accumulator to write to
+      if (INSTR[6]) begin
+        REGS_EN[0] <= 1'b1;
+        REGS_RW[0] <= 1'b0; //write
+      end else begin
+        REGS_EN[1] <= 1'b1;
+        REGS_RW[1] <= 1'b0; //write
+      end
+      uOP <= 3'b111;
+    end
+    // memory operation
+    else
+    begin
+      // store instruction
+      if (INSTR[13] == 1'b1)
+      begin
+        // store A
+        if (INSTR[12] == 1'b0) begin
+          REGS_EN[0] <= 1'b1;
+          REGS_RW[0] <= 1'b1; //read
+        end
+        // store B
+        else begin
+          REGS_EN[1] <= 1'b1;
+          REGS_RW[1] <= 1'b1; //read
+        end
 
-					RAM_EN <= 1'b1;
-					RAM_RW <= 1'b1; // read 
-					uOP <= 3'b111;
-				end
-			end
-		end
-		default:
-		begin
-			uOP <= 3'b111; //woops, missed one somewhere.
-			MAR_EN <= 1'b0;
-		end
+        RAM_EN <= 1'b1;
+        RAM_RW <= 1'b0; // write
+        uOP <= 3'b111;
+      end
+      // load instruction
+      else 
+      begin
+        // load A
+        if (INSTR[12] == 1'b0) begin
+          REGS_EN[0] <= 1'b1;
+          REGS_RW[0] <= 1'b0; // write
+        end
+        // load B
+        else begin
+          REGS_EN[1] <= 1'b1;
+          REGS_RW[1] <= 1'b0; // write
+        end
+
+        RAM_EN <= 1'b1;
+        RAM_RW <= 1'b1; // read 
+        uOP <= 3'b111;
+      end
+    end
+  end
+  default:
+  begin
+    uOP <= 3'b111; //woops, missed one somewhere.
+    MAR_EN <= 1'b0;
+  end
 	endcase
 	end
 end
